52c2c3f622c646732118d329f77c52072b791fdd
lutfiiaziz <muhammadlutfiaziz719@gmail.com>
2024-11-18 12:13:40 +0700

    Baru

--- src/main/java/filtering_app/App.java
+++ src/main/java/filtering_app/App.java
@@ -1,9 +1,13 @@
 package filtering_app;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,23 +17,22 @@ import java.io.IOException;
  */
 public class App extends Application {
 
-    private static Scene scene;
+    // private static Scene scene;
 
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setTitle("Filtering Apps");
+        
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
+    // static void setRoot(String fxml) throws IOException {
+    //     scene.setRoot(loadFXML(fxml));
+    // }
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+    // private static Parent loadFXML(String fxml) throws IOException {
+    //     FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+    //     return fxmlLoader.load();
+    // }
 
     public static void main(String[] args) {
         launch();
